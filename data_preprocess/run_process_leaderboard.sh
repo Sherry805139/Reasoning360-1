5842aec14459a77fb965970e83c0060492009d79
Feng Yao <yaof97@gmail.com>
2025-05-29 08:21:33 +0000

    ready for eval

--- data_preprocess/run_process_leaderboard.sh
+++ data_preprocess/run_process_leaderboard.sh
@@ -3,14 +3,14 @@
 set -e
 
 # Math datasets
-# python data_preprocess/math/dapo_or1_merge_dedup_apr30.py
+python data_preprocess/math/dapo_or1_merge_dedup_apr30.py
 
 # Code datasets
-# python data_preprocess/codegen/humaneval.py
-# python data_preprocess/codegen/humanevalplus.py
-# python data_preprocess/codegen/mbpp.py
-# python data_preprocess/codegen/mbppplus.py
-# python data_preprocess/codegen/livecodebench.py
+python data_preprocess/codegen/humaneval.py
+python data_preprocess/codegen/humanevalplus.py
+python data_preprocess/codegen/mbpp.py
+python data_preprocess/codegen/mbppplus.py
+python data_preprocess/codegen/livecodebench.py
 
 # Stem datasets
 python data_preprocess/stem/gpqa.py
