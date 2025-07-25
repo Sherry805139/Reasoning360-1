01c4492fdb3dee1f67be78948d7dc5d1ce10c9d0
Jianshu She <143899534+Jianshu1only@users.noreply.github.com>
2025-07-25 08:23:25 +0400

    [data] Add IFBench dataset (#113)

--- verl/utils/reward_score/__init__.py
+++ verl/utils/reward_score/__init__.py
@@ -104,6 +104,9 @@ def default_compute_score(
     elif data_source in ["ood__livebench"]:
         from . import livebench
         res = livebench.compute_score(solution_str, ground_truth, extra_info=extra_info)
+    elif data_source in ["ood__ifbench"]:
+        from . import ifbench
+        res = ifbench.compute_score(solution_str, ground_truth, extra_info=extra_info)
     # NOTE: above is added by Reasoning360
     elif data_source == "openai/gsm8k":
         from . import gsm8k
